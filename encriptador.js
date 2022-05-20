c3d2e6595bfdf05ce8451eb5de02f5071b2bf93f
Andres Nicolas Rubio <rubioandres1@gmail.com>
2022-05-20 03:08:18 -0300

    Boton copiar

--- encriptador.js
+++ encriptador.js
@@ -3,10 +3,16 @@ const verTexto = () => {
     (document.getElementById("noMensaje").style.display = "none");
 };
 
-const leerTexto = () => {
+const encriptar = () => {
   let rawText = document.getElementById("textToRead").value;
   verTexto();
   document.getElementById("textResult").innerHTML = encripter(rawText);
+};
+
+const desencriptar = () => {
+  let rawText = document.getElementById("textToRead").value;
+  verTexto();
+  document.getElementById("textResult").innerHTML = decripter(rawText);
 };
 
 const encripter = (text) => {
@@ -38,9 +44,34 @@ const encripter = (text) => {
   return fullText.join("");
 };
 
+const decripter = (text) => {
+  let fullText = text;
+  
+  fullText = fullText.replace(/ai/g,"a");
+  fullText = fullText.replace(/enter/g,"e");
+  fullText = fullText.replace(/imes/g,"i");
+  fullText = fullText.replace(/ober/g,"o");
+  fullText = fullText.replace(/ufat/g,"u");
+   
+  return fullText;
+};
+
+const copyText = () =>{
+    let text = document.getElementById("textResult");
+    text.select()
+    text.setSelectionRange(0, 99999)
+    navigator.clipboard.writeText(text.value);
+
+    alert("Copied the text: " + text.value);
+
+}
+
+
 //Acciones
 
-document.getElementById("botonEncriptar").addEventListener("click", leerTexto);
+document.getElementById("botonEncriptar").addEventListener("click", encriptar);
+document.getElementById("botonDesencriptar").addEventListener("click", desencriptar);
+document.getElementById("botonCopiar").addEventListener("click", copyText);
 
 //Para seleccionar el texto interno del text area
 //document.getElementById("textToRead").select();
